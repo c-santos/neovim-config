966ece35b1e462d59660b060bec14a34199bf383
c-santos <jcms121899@gmail.com>
2024-06-23 16:18:41 +0800

    added remaps and vim options

--- lua/remap.lua
+++ lua/remap.lua
@@ -1,3 +1,12 @@
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>e", ":NvimTreeFocus<CR>")
 vim.keymap.set("n", "<leader>b", ":NvimTreeToggle<CR>")
+
+
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+vim.keymap.set("x", "<leader>p", "\"_dP")
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -3,3 +3,48 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=4")
 vim.cmd("set softtabstop=4")
 vim.cmd("set shiftwidth=4")
+
+vim.opt.incsearch = true
+
+vim.opt.smartindent = true
+vim.opt.termguicolors = true
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+
+vim.opt.updatetime = 50
+
+vim.opt.colorcolumn = "80"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
