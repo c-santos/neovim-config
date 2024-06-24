efee8add3b32e6a843751f2e9524824d8400986d
c-santos <jcms121899@gmail.com>
2024-06-24 21:21:54 +0800

    added jk escape keymap from insert mode and added comments

--- lua/remap.lua
+++ lua/remap.lua
@@ -2,12 +2,26 @@ local noremap = function (mode, keys, cmd)
    vim.keymap.set(mode, keys, cmd) 
 end
 
+--- map leader
 vim.g.mapleader = " "
 
+
+--- netrw
 noremap("n", "<leader>pv", ":Ex<CR>")
+
+--- move highlighted with cursor
 noremap("v", "J", ":m '>+1<CR>gv=gv")
 noremap("v", "K", ":m '<-2<CR>gv=gv")
+
+--- center cursor when nexting search
 noremap("n", "n", "nzzzv")
 noremap("n", "N", "Nzzzv")
+
+--- when pasting over a highlighted text, retains the yanked thing
 noremap("x", "<leader>p", "\"_dP")
+
+--- substitute all instances of word on cursor
 noremap("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
+--- exit insert mode with 'jk'
+vim.keymap.set('i', 'jk', '<Esc>', { noremap = true, silent = true })
