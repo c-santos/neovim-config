68972f231f90aee711b6acd532b951b79e426e32
jcsantos <jcsantos@ubx.ph>
2024-06-24 09:55:50 +0800

    fixed more formatting

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -24,9 +24,11 @@ return {
             local lspconfig = require("lspconfig")
             lspconfig.lua_ls.setup({})
             lspconfig.tsserver.setup({})
+
             vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
             vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
             vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, {})
+
         end
     },
     {
