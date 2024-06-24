df2abfc97a4ac3989e9dcd7145a59a61f754a482
jcsantos <jcms121899@gmail.com>
2024-06-24 11:09:02 +0800

    Added telescope live grep

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,9 +1,10 @@
 return {
     'nvim-telescope/telescope.nvim', tag = '0.1.8',
-    dependencies = { 'nvim-lua/plenary.nvim' },
+    dependencies = { 'nvim-lua/plenary.nvim', 'BurntSushi/ripgrep' },
     config = function()
         local builtin = require("telescope.builtin")
         vim.keymap.set("n", "<C-p>", builtin.git_files, {})
         vim.keymap.set("n", "<leader>f", builtin.find_files, {})
+        vim.keymap.set("n", "<leader>ps", builtin.live_grep, {})
     end
 }
