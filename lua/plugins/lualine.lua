441238e3184d8cc81446cf91b6de5c103aac0807
c-santos <jcms121899@gmail.com>
2024-06-24 23:41:14 +0800

    rename lualine.lua

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -2,7 +2,7 @@ return {
     "nvim-lualine/lualine.nvim",
     config = function()
         require('lualine').setup({
-            options = { theme = "tokyonight" },
+            options = { theme = "auto" },
         })
     end
 }
