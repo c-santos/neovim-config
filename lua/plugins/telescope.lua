e7dcc2ae4a63796f5a9e5dd8a20cf8be3cb59271
jcsantos <jcms121899@gmail.com>
2024-06-27 08:47:19 +0800

    feat: added word on cursor grep

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -6,6 +6,14 @@ return {
         vim.keymap.set("n", "<C-p>", builtin.git_files, {})
         vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
         vim.keymap.set("n", "<leader>ps", builtin.live_grep, {})
+        vim.keymap.set("n", "<leader>pws", function ()
+            local word = vim.fn.expand("<cword>")
+            builtin.grep_string({ search = word })
+        end, {})
+        vim.keymap.set("n", "<leader>pWs", function ()
+            local word = vim.fn.expand("<cWORD>")
+            builtin.grep_string({ search = word })
+        end, {})
         vim.keymap.set("n", "gr", builtin.lsp_references, {})
     end
 }
