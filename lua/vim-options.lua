6e744fe87652fb3eb16699158a8c32bedee73219
jcsantos <jcms121899@gmail.com>
2024-07-02 22:41:43 +0800

    feat: added usage of sys clipboard

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -4,6 +4,7 @@ vim.cmd("set tabstop=4")
 vim.cmd("set softtabstop=4")
 vim.cmd("set shiftwidth=4")
 vim.cmd("set linebreak")
+vim.cmd("set clipboard=unnamedplus")
 
 vim.g.netrw_bufsettings = 'noma nomod nu rnu nobl nowrap ro' -- netrw line num
 vim.g.have_nerd_font = true
