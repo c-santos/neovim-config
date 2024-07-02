2c38618a5b27fa41e01c9ef5b1e3428907bd7562
jcsantos <jcms121899@gmail.com>
2024-07-02 22:42:03 +0800

    chore: added dep for lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,5 +1,8 @@
 return {
     "nvim-lualine/lualine.nvim",
+    dependecies = {
+        'nvim-tree/nvim-web-devicons'
+    },
     config = function()
         require('lualine').setup({
             options = { theme = "auto" },
